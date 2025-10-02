76a755eac1a5e9a6770cd77d51a51157833ccc0a
gizatiara334-lab <gizatiara334@gmail.com>
2025-10-02 12:54:57 +0700

    Create variabel.php

---
+++ LATIHAN LATIHAN/variabel.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Contoh Script PHP</title>
+</head>
+<body>
+    <?php
+    $nama = "Giza Aurelya nadine najunda tiara sofiyahya";
+    echo "$nama berkata \"Katakan namamu!\"";
+    ?>
+</body>
+</html>
